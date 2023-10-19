7e0165358b9f409c9ef6bd48441d0c1ca59f1f65
Nastia <adruts@mail.ru>
2023-10-19 08:30:13 +0300

    https://github.com/anastyadru/Classandobject.git

--- Assets/Scripts/AddStudentPanelController.cs
+++ Assets/Scripts/AddStudentPanelController.cs
@@ -17,33 +17,26 @@ public class AddStudentPanelController : MonoBehaviour
     public InputField groupInput;
     public MenuController menuController;
 
-    public void AddStudentButtonClicked()
+    public void OnAddButtonClicked()
     {
-        if (surnameInput != null && nameInput != null && patronymicInput != null && birthInput != null &&
-            facultyInput != null && courseInput != null && groupInput != null)
-        {
-            var surname = surnameInput.text;
-            var name = nameInput.text;
-            var patronymic = patronymicInput.text;
-            var birth = DateTime.ParseExact(birthInput.text, "dd.MM.yyyy", null);
-            var faculty = facultyInput.text;
-            int course = int.Parse(courseInput.text);
-            int group = int.Parse(groupInput.text);
+        string surname = surnameInput.text;
+        string name = nameInput.text;
+        string patronymic = patronymicInput.text;
+        DateTime birth = Convert.ToDateTime(birthInput.text);
+        string faculty = facultyInput.text;
+        int course = int.Parse(courseInput.text);
+        int group = int.Parse(groupInput.text);
 
-            if (menuController != null) 
-            {
-                menuController.AddStudent(surname, name, patronymic, birth, faculty, course, group);
-            }
-            
-            else 
-            {
-                Debug.LogError("menuController is null");
-            }
-        }
-        
-        else
-        {
-            Debug.LogError("One or more input fields are null");
-        }
+        menuController.AddStudent(surname, name, patronymic, birth, faculty, course, group);
+
+        surnameInput.text = "";
+        nameInput.text = "";
+        patronymicInput.text = "";
+        birthInput.text = "";
+        facultyInput.text = "";
+        courseInput.text = "";
+        groupInput.text = "";
+
+        gameObject.SetActive(false);
     }
 }
