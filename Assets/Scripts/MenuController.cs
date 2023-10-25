627a615080ee89dadf5b6f19686ef59d79d22b9e
Nastia <adruts@mail.ru>
2023-10-25 16:29:13 +0300

    https://github.com/anastyadru/Classandobject.git

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -33,41 +33,6 @@ public class MenuController : MonoBehaviour
         deletePersonPanel.SetActive(true);
     }
 
-    public void AddStudent(string surname, string name, string patronymic, DateTime birth, string faculty, int course, int group)
-    {
-        var student = new Student(surname, name, patronymic, birth, faculty, course, group);
-        people.Add(student);
-        Debug.Log("Студент добавлен:\n" + student.Print());
-    }
-
-    public void EditStudent(string surname)
-    {
-        var studentToEdit = people.Find(p => p.Surname == surname);
-
-        if (studentToEdit != null)
-        {
-            studentToEdit.Edit();
-            Debug.Log("Данные студента изменены:\n" + studentToEdit.Print());
-        }
-        else
-        {
-            Debug.Log("Студент с такой фамилией не найден");
-        }
-    }
-
-    public void DeleteStudent(string surname)
-    {
-        var studentToDelete = people.Find(p => p.Surname == surname);
-
-        if (studentToDelete != null)
-        {
-            people.Remove(studentToDelete);
-            Debug.Log("Данные студента удалены");
-        }
-        else
-        {
-            Debug.Log("Студент с такой фамилией не найден");
-        }
-    }
+    
 
 }
