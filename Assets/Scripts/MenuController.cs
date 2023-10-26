74874350c6d0059cea195bd8c944a10f63079d93
Nastia <adruts@mail.ru>
2023-10-26 15:01:55 +0300

    https://github.com/anastyadru/Classandobject.git

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
@@ -9,6 +10,23 @@ public class MenuController : MonoBehaviour
     public GameObject addPersonPanel;
     public GameObject editPersonPanel;
     public GameObject deletePersonPanel;
+    
+    public Text surnameInput;
+    public Text nameInput;
+    public Text patronymicInput;
+    public Text birthInput;
+    public Text personDropdown;
+
+    public Text facultyInput;
+    public Text courseInput;
+    public Text groupInput;
+
+    public Text companyInput;
+    public Text salaryInput;
+    public Text experienceInput;
+
+    public Text brandInput;
+    public Text modelInput;
 
     private List<Human> people = new List<Human>();
 
@@ -33,84 +51,52 @@ public class MenuController : MonoBehaviour
         deletePersonPanel.SetActive(true);
     }
 
-    static void AddPerson(List<Human> people)
+    public AddPerson()
     {
-        Console.WriteLine("Введите фамилию: ");
-        var surname = Console.ReadLine();
+        string surname = surnameInput.text;
+        string name = nameInput.text;
+        string patronymic = patronymicInput.text;
+        string birthString = birthInput.text;
+        var birth = System.DateTime.Parse(birthString);
 
-        Console.WriteLine("Введите имя: ");
-        var name = Console.ReadLine();
+        int personType = personDropdown.value;
 
-        Console.WriteLine("Введите отчество: ");
-        var patronymic = Console.ReadLine();
-
-        Console.WriteLine("Введите дату рождения в формате ДД.ММ.ГГГГ: ");
-        var birth = Convert.ToDateTime(Console.ReadLine());
-
-        Console.Write("Выберите, кого хотите добавить:\n1) student \n2) employee \n3) driver \nВаше решение: ");
-        var person = int.Parse(Console.ReadLine());
-
-        switch (person)
+        if (personType == 0)
         {
-            case 1:
+            string faculty = facultyInput.text;
+            int course = int.Parse(courseInput.text);
+            int group = int.Parse(groupInput.text);
 
-                Console.Write("Введите факультет: ");
-                var faculty = Console.ReadLine();
+            Student student = new Student(surname, name, patronymic, birth, faculty, course, group);
+            people.Add(student);
 
-                Console.Write("Введите курс: ");
-                var course = int.Parse(Console.ReadLine());
+            Debug.Log("Данные студента добавлены:\n" + student.Print());
+        }
+        
+        else if (personType == 1)
+        {
+            string company = companyInput.text;
+            int salary = int.Parse(salaryInput.text);
+            int experience = int.Parse(experienceInput.text);
+            
+            Employee employee = new Employee(surname, name, patronymic, birth, company, salary, experience);
+            people.Add(employee);
+            
+            Debug.Log("Данные работника добавлены:\n" + employee.Print());
+        }
+        
+        else if (personType == 2)
+        {
+            string company = companyInput.text;
+            int salary = int.Parse(salaryInput.text);
+            int experience = int.Parse(experienceInput.text);
+            string brand = brandInput.text;
+            string model = modelInput.text;
 
-                Console.Write("Введите группу: ");
-                var group = int.Parse(Console.ReadLine());
+            Driver driver = new Driver(surname, name, patronymic, birth, company, salary, experience, brand, model);
+            people.Add(driver);
 
-                Student student = new Student(surname, name, patronymic, birth, faculty, course, group);
-                people.Add(student);
-
-                Console.WriteLine("Данные студента добавлены:\n" + student.Print());
-
-                break;
-
-            case 2:
-
-                Console.Write("Введите название компании: ");
-                var company = Console.ReadLine();
-
-                Console.Write("Введите заработную плату: ");
-                var salary = int.Parse(Console.ReadLine());
-
-                Console.Write("Введите опыт работы: ");
-                var experience = int.Parse(Console.ReadLine());
-
-                Employee employee = new Employee(surname, name, patronymic, birth, company, salary, experience);
-                people.Add(employee);
-
-                Console.WriteLine("Данные работника добавлены:\n" + employee.Print());
-
-                break;
-
-            case 3:
-
-                Console.Write("Введите название компании: ");
-                var company3 = Console.ReadLine();
-
-                Console.Write("Введите заработную плату: ");
-                var salary3 = int.Parse(Console.ReadLine());
-
-                Console.Write("Введите опыт работы: ");
-                var experience3 = int.Parse(Console.ReadLine());
-
-                Console.Write("Введите бренд: ");
-                var brand = Console.ReadLine();
-
-                Console.Write("Введите модель: ");
-                var model = Console.ReadLine();
-
-                Driver driver = new Driver(surname, name, patronymic, birth, company3, salary3, experience3, brand, model);
-                people.Add(driver);
-
-                Console.WriteLine("Данные водителя добавлены:\n" + driver.Print());
-
-                break;
+            Debug.Log("Данные водителя добавлены:\n" + driver.Print());
         }
     }
         
@@ -124,12 +110,12 @@ public class MenuController : MonoBehaviour
         if (personToEdit != null)
         {
             personToEdit.Edit();
-            Console.WriteLine("Изменения сохранены");
+            Debug.Log("Изменения сохранены");
         }
                     
         else
         {
-            Console.WriteLine("Человек с такой фамилией не найден");
+            Debug.Log("Человек с такой фамилией не найден");
         }
     }
         
@@ -143,12 +129,12 @@ public class MenuController : MonoBehaviour
         if (personToDelete != null)
         {
             people.Remove(personToDelete);
-            Console.WriteLine("Информация о человеке удалена");
+            Debug.Log("Информация о человеке удалена");
         }
         
         else
         {
-            Console.WriteLine("Человек с такой фамилией не найден");
+            Debug.Log("Человек с такой фамилией не найден");
         }
     }
 
