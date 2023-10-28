df11c13813d68101ad98fdea0dc58881f4d9db6b
Nastia <adruts@mail.ru>
2023-10-28 20:05:42 +0300

    https://github.com/anastyadru/Classandobject.git

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -69,6 +69,18 @@ public class MenuController : MonoBehaviour
             string faculty = facultyInput.text;
             int course = int.Parse(courseInput.text);
             int group = int.Parse(groupInput.text);
+            
+            if (studentsList == null)
+            {
+                Debug.LogError("studentsList не был инициализирован!");
+                return;
+            }
+            
+            if (string.IsNullOrEmpty(faculty) || string.IsNullOrEmpty(courseInput.text) || string.IsNullOrEmpty(groupInput.text))
+            {
+                Debug.LogError("Не все поля студента заполнены!");
+                return;
+            }
 
             Student student = new Student(surname, name, patronymic, birth, faculty, course, group);
             people.Add(student);
@@ -81,6 +93,13 @@ public class MenuController : MonoBehaviour
             string company = companyInput.text;
             int salary = int.Parse(salaryInput.text);
             int experience = int.Parse(experienceInput.text);
+            
+            
+            if (string.IsNullOrEmpty(company) || string.IsNullOrEmpty(salaryInput.text) || string.IsNullOrEmpty(experienceInput.text))
+            {
+                Debug.LogError("Не все поля работника заполнены!");
+                return;
+            }
             
             Employee employee = new Employee(surname, name, patronymic, birth, company, salary, experience);
             people.Add(employee);
@@ -95,6 +114,12 @@ public class MenuController : MonoBehaviour
             int experience3 = int.Parse(experienceInput.text);
             string brand = brandInput.text;
             string model = modelInput.text;
+            
+            if (string.IsNullOrEmpty(company3) || string.IsNullOrEmpty(salaryInput.text) || string.IsNullOrEmpty(experienceInput.text) || string.IsNullOrEmpty(brand) || string.IsNullOrEmpty(model))
+            {
+                Debug.LogError("Не все поля водителя заполнены!");
+                return;
+            }
 
             Driver driver = new Driver(surname, name, patronymic, birth, company3, salary3, experience3, brand, model);
             people.Add(driver);
